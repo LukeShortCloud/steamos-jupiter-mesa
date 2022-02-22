upcliujie/mesa

nir: use a GC context for instructions
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

src/compiler/nir/nir.c
@@ -100,17 +100,6 @@ nir_component_mask_reinterpret(nir_component_mask_t mask,
    return new_mask;
 }
 
-static void
-nir_shader_destructor(void *ptr)
-{
-   nir_shader *shader = ptr;
-
-   /* Free all instrs from the shader, since they're not ralloced. */
-   list_for_each_entry_safe(nir_instr, instr, &shader->gc_list, gc_node) {
-      nir_instr_free(instr);
-   }
-}
-
 nir_shader *
 nir_shader_create(void *mem_ctx,
                   gl_shader_stage stage,
@@ -118,7 +107,8 @@ nir_shader_create(void *mem_ctx,
                   shader_info *si)
 {
    nir_shader *shader = rzalloc(mem_ctx, nir_shader);
-   ralloc_set_destructor(shader, nir_shader_destructor);
+
+   shader->gctx = gc_context(shader);
 
    exec_list_make_empty(&shader->variables);
 
@@ -132,8 +122,6 @@ nir_shader_create(void *mem_ctx,
    }
 
    exec_list_make_empty(&shader->functions);
-
-   list_inithead(&shader->gc_list);
 
    shader->num_inputs = 0;
    shader->num_outputs = 0;
@@ -348,7 +336,7 @@ static void src_free_indirects(nir_src *src)
 {
    if (src_has_indirect(src)) {
       assert(src->reg.indirect->is_ssa || !src->reg.indirect->reg.indirect);
-      free(src->reg.indirect);
+      gc_free(src->reg.indirect);
       src->reg.indirect = NULL;
    }
 }
@@ -357,13 +345,13 @@ static void dest_free_indirects(nir_dest *dest)
 {
    if (!dest->is_ssa && dest->reg.indirect) {
       assert(dest->reg.indirect->is_ssa || !dest->reg.indirect->reg.indirect);
-      free(dest->reg.indirect);
+      gc_free(dest->reg.indirect);
       dest->reg.indirect = NULL;
    }
 }
 
 static void
-src_copy(nir_src *dest, const nir_src *src)
+src_copy(nir_src *dest, const nir_src *src, gc_ctx *ctx)
 {
    src_free_indirects(dest);
 
@@ -374,8 +362,8 @@ src_copy(nir_src *dest, const nir_src *src)
       dest->reg.base_offset = src->reg.base_offset;
       dest->reg.reg = src->reg.reg;
       if (src->reg.indirect) {
-         dest->reg.indirect = malloc(sizeof(nir_src));
-         src_copy(dest->reg.indirect, src->reg.indirect);
+         dest->reg.indirect = gc_alloc(ctx, nir_src, 1);
+         src_copy(dest->reg.indirect, src->reg.indirect, ctx);
       } else {
          dest->reg.indirect = NULL;
       }
@@ -387,7 +375,7 @@ src_copy(nir_src *dest, const nir_src *src)
  */
 void nir_src_copy(nir_src *dest, const nir_src *src, nir_instr *instr)
 {
-   src_copy(dest, src);
+   src_copy(dest, src, instr ? gc_get_context(instr) : NULL);
 }
 
 void nir_dest_copy(nir_dest *dest, const nir_dest *src, nir_instr *instr)
@@ -402,7 +390,7 @@ void nir_dest_copy(nir_dest *dest, const nir_dest *src, nir_instr *instr)
    dest->reg.base_offset = src->reg.base_offset;
    dest->reg.reg = src->reg.reg;
    if (src->reg.indirect) {
-      dest->reg.indirect = malloc(sizeof(nir_src));
+      dest->reg.indirect = gc_alloc(gc_get_context(instr), nir_src, 1);
       nir_src_copy(dest->reg.indirect, src->reg.indirect, instr);
    } else {
       dest->reg.indirect = NULL;
@@ -613,7 +601,7 @@ nir_alu_instr_create(nir_shader *shader, nir_op op)
 {
    unsigned num_srcs = nir_op_infos[op].num_inputs;
    /* TODO: don't use calloc */
-   nir_alu_instr *instr = calloc(1, sizeof(nir_alu_instr) + num_srcs * sizeof(nir_alu_src));
+   nir_alu_instr *instr = gc_zalloc_zla(shader->gctx, nir_alu_instr, nir_alu_src, num_srcs);
 
    instr_init(&instr->instr, nir_instr_type_alu);
    instr->op = op;
@@ -621,15 +609,13 @@ nir_alu_instr_create(nir_shader *shader, nir_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       alu_src_init(&instr->src[i]);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
 nir_deref_instr *
 nir_deref_instr_create(nir_shader *shader, nir_deref_type deref_type)
 {
-   nir_deref_instr *instr = calloc(1, sizeof(*instr));
+   nir_deref_instr *instr = gc_zalloc(shader->gctx, nir_deref_instr, 1);
 
    instr_init(&instr->instr, nir_instr_type_deref);
 
@@ -643,22 +629,18 @@ nir_deref_instr_create(nir_shader *shader, nir_deref_type deref_type)
 
    dest_init(&instr->dest);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
 nir_jump_instr *
 nir_jump_instr_create(nir_shader *shader, nir_jump_type type)
 {
-   nir_jump_instr *instr = malloc(sizeof(*instr));
+   nir_jump_instr *instr = gc_alloc(shader->gctx, nir_jump_instr, 1);
    instr_init(&instr->instr, nir_instr_type_jump);
    src_init(&instr->condition);
    instr->type = type;
    instr->target = NULL;
    instr->else_target = NULL;
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -668,12 +650,10 @@ nir_load_const_instr_create(nir_shader *shader, unsigned num_components,
                             unsigned bit_size)
 {
    nir_load_const_instr *instr =
-      calloc(1, sizeof(*instr) + num_components * sizeof(*instr->value));
+      gc_zalloc_zla(shader->gctx, nir_load_const_instr, nir_const_value, num_components);
    instr_init(&instr->instr, nir_instr_type_load_const);
 
    nir_ssa_def_init(&instr->instr, &instr->def, num_components, bit_size);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -684,7 +664,7 @@ nir_intrinsic_instr_create(nir_shader *shader, nir_intrinsic_op op)
    unsigned num_srcs = nir_intrinsic_infos[op].num_srcs;
    /* TODO: don't use calloc */
    nir_intrinsic_instr *instr =
-      calloc(1, sizeof(nir_intrinsic_instr) + num_srcs * sizeof(nir_src));
+      gc_zalloc_zla(shader->gctx, nir_intrinsic_instr, nir_src, num_srcs);
 
    instr_init(&instr->instr, nir_instr_type_intrinsic);
    instr->intrinsic = op;
@@ -695,8 +675,6 @@ nir_intrinsic_instr_create(nir_shader *shader, nir_intrinsic_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i]);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
@@ -705,15 +683,13 @@ nir_call_instr_create(nir_shader *shader, nir_function *callee)
 {
    const unsigned num_params = callee->num_params;
    nir_call_instr *instr =
-      calloc(1, sizeof(*instr) + num_params * sizeof(instr->params[0]));
+      gc_zalloc_zla(shader->gctx, nir_call_instr, nir_src, num_params);
 
    instr_init(&instr->instr, nir_instr_type_call);
    instr->callee = callee;
    instr->num_params = num_params;
    for (unsigned i = 0; i < num_params; i++)
       src_init(&instr->params[i]);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -729,21 +705,19 @@ static int8_t default_tg4_offsets[4][2] =
 nir_tex_instr *
 nir_tex_instr_create(nir_shader *shader, unsigned num_srcs)
 {
-   nir_tex_instr *instr = calloc(1, sizeof(*instr));
+   nir_tex_instr *instr = gc_zalloc(shader->gctx, nir_tex_instr, 1);
    instr_init(&instr->instr, nir_instr_type_tex);
 
    dest_init(&instr->dest);
 
    instr->num_srcs = num_srcs;
-   instr->src = malloc(sizeof(nir_tex_src) * num_srcs);
+   instr->src = gc_alloc(shader->gctx, nir_tex_src, num_srcs);
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i].src);
 
    instr->texture_index = 0;
    instr->sampler_index = 0;
    memcpy(instr->tg4_offsets, default_tg4_offsets, sizeof(instr->tg4_offsets));
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -753,8 +727,7 @@ nir_tex_instr_add_src(nir_tex_instr *tex,
                       nir_tex_src_type src_type,
                       nir_src src)
 {
-   nir_tex_src *new_srcs = calloc(sizeof(*new_srcs),
-                                         tex->num_srcs + 1);
+   nir_tex_src *new_srcs = gc_zalloc(gc_get_context(tex), nir_tex_src, tex->num_srcs + 1);
 
    for (unsigned i = 0; i < tex->num_srcs; i++) {
       new_srcs[i].src_type = tex->src[i].src_type;
@@ -762,7 +735,7 @@ nir_tex_instr_add_src(nir_tex_instr *tex,
                          &tex->src[i].src);
    }
 
-   free(tex->src);
+   gc_free(tex->src);
    tex->src = new_srcs;
 
    tex->src[tex->num_srcs].src_type = src_type;
@@ -798,13 +771,11 @@ nir_tex_instr_has_explicit_tg4_offsets(nir_tex_instr *tex)
 nir_phi_instr *
 nir_phi_instr_create(nir_shader *shader)
 {
-   nir_phi_instr *instr = malloc(sizeof(*instr));
+   nir_phi_instr *instr = gc_alloc(shader->gctx, nir_phi_instr, 1);
    instr_init(&instr->instr, nir_instr_type_phi);
 
    dest_init(&instr->dest);
    exec_list_make_empty(&instr->srcs);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -822,7 +793,7 @@ nir_phi_instr_add_src(nir_phi_instr *instr, nir_block *pred, nir_src src)
 {
    nir_phi_src *phi_src;
 
-   phi_src = calloc(1, sizeof(nir_phi_src));
+   phi_src = gc_zalloc(gc_get_context(instr), nir_phi_src, 1);
    phi_src->pred = pred;
    phi_src->src = src;
    phi_src->src.parent_instr = &instr->instr;
@@ -834,12 +805,10 @@ nir_phi_instr_add_src(nir_phi_instr *instr, nir_block *pred, nir_src src)
 nir_parallel_copy_instr *
 nir_parallel_copy_instr_create(nir_shader *shader)
 {
-   nir_parallel_copy_instr *instr = malloc(sizeof(*instr));
+   nir_parallel_copy_instr *instr = gc_alloc(shader->gctx, nir_parallel_copy_instr, 1);
    instr_init(&instr->instr, nir_instr_type_parallel_copy);
 
    exec_list_make_empty(&instr->entries);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -849,12 +818,10 @@ nir_ssa_undef_instr_create(nir_shader *shader,
                            unsigned num_components,
                            unsigned bit_size)
 {
-   nir_ssa_undef_instr *instr = malloc(sizeof(*instr));
+   nir_ssa_undef_instr *instr = gc_alloc(shader->gctx, nir_ssa_undef_instr, 1);
    instr_init(&instr->instr, nir_instr_type_ssa_undef);
 
    nir_ssa_def_init(&instr->instr, &instr->def, num_components, bit_size);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -1174,14 +1141,13 @@ void nir_instr_free(nir_instr *instr)
 
    switch (instr->type) {
    case nir_instr_type_tex:
-      free(nir_instr_as_tex(instr)->src);
+      gc_free(nir_instr_as_tex(instr)->src);
       break;
 
    case nir_instr_type_phi: {
       nir_phi_instr *phi = nir_instr_as_phi(instr);
-      nir_foreach_phi_src_safe(phi_src, phi) {
-         free(phi_src);
-      }
+      nir_foreach_phi_src_safe(phi_src, phi)
+         gc_free(phi_src);
       break;
    }
 
@@ -1189,8 +1155,7 @@ void nir_instr_free(nir_instr *instr)
       break;
    }
 
-   list_del(&instr->gc_node);
-   free(instr);
+   gc_free(instr);
 }
 
 void
@@ -1578,11 +1543,12 @@ nir_instr_move_src(nir_instr *dest_instr, nir_src *dest, nir_src *src)
 void
 nir_if_rewrite_condition(nir_if *if_stmt, nir_src new_src)
 {
+   nir_shader *shader = ralloc_parent(if_stmt);
    nir_src *src = &if_stmt->condition;
    assert(!src_is_valid(src) || src->parent_if == if_stmt);
 
    src_remove_all_uses(src);
-   src_copy(src, &new_src);
+   src_copy(src, &new_src, shader->gctx);
    src_add_all_uses(src, NULL, if_stmt);
 }
 

LukeShortCloud/steamos-jupiter-mesa

src/compiler/nir/nir.c
@@ -51,8 +51,6 @@ static const struct debug_named_value nir_debug_control[] = {
      "Disable shader validation at each successful lowering/optimization call" },
    { "validate_ssa_dominance", NIR_DEBUG_VALIDATE_SSA_DOMINANCE,
      "Validate SSA dominance in shader at each successful lowering/optimization call" },
-   { "validate_gc_list", NIR_DEBUG_VALIDATE_GC_LIST,
-     "Validate the instruction GC list at each successful lowering/optimization call" },
    { "tgsi", NIR_DEBUG_TGSI,
      "Dump NIR/TGSI shaders when doing a NIR<->TGSI translation" },
    { "print", NIR_DEBUG_PRINT,
@@ -184,17 +182,6 @@ nir_component_mask_reinterpret(nir_component_mask_t mask,
    return new_mask;
 }
 
-static void
-nir_shader_destructor(void *ptr)
-{
-   nir_shader *shader = ptr;
-
-   /* Free all instrs from the shader, since they're not ralloced. */
-   list_for_each_entry_safe(nir_instr, instr, &shader->gc_list, gc_node) {
-      nir_instr_free(instr);
-   }
-}
-
 nir_shader *
 nir_shader_create(void *mem_ctx,
                   gl_shader_stage stage,
@@ -202,7 +189,8 @@ nir_shader_create(void *mem_ctx,
                   shader_info *si)
 {
    nir_shader *shader = rzalloc(mem_ctx, nir_shader);
-   ralloc_set_destructor(shader, nir_shader_destructor);
+
+   shader->gctx = gc_context(shader);
 
 #ifndef NDEBUG
    nir_process_debug_variable();
@@ -220,8 +208,6 @@ nir_shader_create(void *mem_ctx,
    }
 
    exec_list_make_empty(&shader->functions);
-
-   list_inithead(&shader->gc_list);
 
    shader->num_inputs = 0;
    shader->num_outputs = 0;
@@ -438,7 +424,7 @@ static void src_free_indirects(nir_src *src)
 {
    if (src_has_indirect(src)) {
       assert(src->reg.indirect->is_ssa || !src->reg.indirect->reg.indirect);
-      free(src->reg.indirect);
+      gc_free(src->reg.indirect);
       src->reg.indirect = NULL;
    }
 }
@@ -447,13 +433,13 @@ static void dest_free_indirects(nir_dest *dest)
 {
    if (!dest->is_ssa && dest->reg.indirect) {
       assert(dest->reg.indirect->is_ssa || !dest->reg.indirect->reg.indirect);
-      free(dest->reg.indirect);
+      gc_free(dest->reg.indirect);
       dest->reg.indirect = NULL;
    }
 }
 
 static void
-src_copy(nir_src *dest, const nir_src *src)
+src_copy(nir_src *dest, const nir_src *src, gc_ctx *ctx)
 {
    src_free_indirects(dest);
 
@@ -464,8 +450,8 @@ src_copy(nir_src *dest, const nir_src *src)
       dest->reg.base_offset = src->reg.base_offset;
       dest->reg.reg = src->reg.reg;
       if (src->reg.indirect) {
-         dest->reg.indirect = calloc(1, sizeof(nir_src));
-         src_copy(dest->reg.indirect, src->reg.indirect);
+         dest->reg.indirect = gc_zalloc(ctx, nir_src, 1);
+         src_copy(dest->reg.indirect, src->reg.indirect, ctx);
       } else {
          dest->reg.indirect = NULL;
       }
@@ -477,7 +463,7 @@ src_copy(nir_src *dest, const nir_src *src)
  */
 void nir_src_copy(nir_src *dest, const nir_src *src, nir_instr *instr)
 {
-   src_copy(dest, src);
+   src_copy(dest, src, instr ? gc_get_context(instr) : NULL);
 }
 
 void nir_dest_copy(nir_dest *dest, const nir_dest *src, nir_instr *instr)
@@ -492,7 +478,7 @@ void nir_dest_copy(nir_dest *dest, const nir_dest *src, nir_instr *instr)
    dest->reg.base_offset = src->reg.base_offset;
    dest->reg.reg = src->reg.reg;
    if (src->reg.indirect) {
-      dest->reg.indirect = calloc(1, sizeof(nir_src));
+      dest->reg.indirect = gc_zalloc(gc_get_context(instr), nir_src, 1);
       nir_src_copy(dest->reg.indirect, src->reg.indirect, instr);
    } else {
       dest->reg.indirect = NULL;
@@ -703,7 +689,7 @@ nir_alu_instr_create(nir_shader *shader, nir_op op)
 {
    unsigned num_srcs = nir_op_infos[op].num_inputs;
    /* TODO: don't use calloc */
-   nir_alu_instr *instr = calloc(1, sizeof(nir_alu_instr) + num_srcs * sizeof(nir_alu_src));
+   nir_alu_instr *instr = gc_zalloc_zla(shader->gctx, nir_alu_instr, nir_alu_src, num_srcs);
 
    instr_init(&instr->instr, nir_instr_type_alu);
    instr->op = op;
@@ -711,15 +697,13 @@ nir_alu_instr_create(nir_shader *shader, nir_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       alu_src_init(&instr->src[i]);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
 nir_deref_instr *
 nir_deref_instr_create(nir_shader *shader, nir_deref_type deref_type)
 {
-   nir_deref_instr *instr = calloc(1, sizeof(*instr));
+   nir_deref_instr *instr = gc_zalloc(shader->gctx, nir_deref_instr, 1);
 
    instr_init(&instr->instr, nir_instr_type_deref);
 
@@ -733,22 +717,18 @@ nir_deref_instr_create(nir_shader *shader, nir_deref_type deref_type)
 
    dest_init(&instr->dest);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
 nir_jump_instr *
 nir_jump_instr_create(nir_shader *shader, nir_jump_type type)
 {
-   nir_jump_instr *instr = malloc(sizeof(*instr));
+   nir_jump_instr *instr = gc_alloc(shader->gctx, nir_jump_instr, 1);
    instr_init(&instr->instr, nir_instr_type_jump);
    src_init(&instr->condition);
    instr->type = type;
    instr->target = NULL;
    instr->else_target = NULL;
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -758,12 +738,10 @@ nir_load_const_instr_create(nir_shader *shader, unsigned num_components,
                             unsigned bit_size)
 {
    nir_load_const_instr *instr =
-      calloc(1, sizeof(*instr) + num_components * sizeof(*instr->value));
+      gc_zalloc_zla(shader->gctx, nir_load_const_instr, nir_const_value, num_components);
    instr_init(&instr->instr, nir_instr_type_load_const);
 
    nir_ssa_def_init(&instr->instr, &instr->def, num_components, bit_size);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -774,7 +752,7 @@ nir_intrinsic_instr_create(nir_shader *shader, nir_intrinsic_op op)
    unsigned num_srcs = nir_intrinsic_infos[op].num_srcs;
    /* TODO: don't use calloc */
    nir_intrinsic_instr *instr =
-      calloc(1, sizeof(nir_intrinsic_instr) + num_srcs * sizeof(nir_src));
+      gc_zalloc_zla(shader->gctx, nir_intrinsic_instr, nir_src, num_srcs);
 
    instr_init(&instr->instr, nir_instr_type_intrinsic);
    instr->intrinsic = op;
@@ -785,8 +763,6 @@ nir_intrinsic_instr_create(nir_shader *shader, nir_intrinsic_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i]);
 
-   list_add(&instr->instr.gc_node, &shader->gc_list);
-
    return instr;
 }
 
@@ -795,15 +771,13 @@ nir_call_instr_create(nir_shader *shader, nir_function *callee)
 {
    const unsigned num_params = callee->num_params;
    nir_call_instr *instr =
-      calloc(1, sizeof(*instr) + num_params * sizeof(instr->params[0]));
+      gc_zalloc_zla(shader->gctx, nir_call_instr, nir_src, num_params);
 
    instr_init(&instr->instr, nir_instr_type_call);
    instr->callee = callee;
    instr->num_params = num_params;
    for (unsigned i = 0; i < num_params; i++)
       src_init(&instr->params[i]);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -819,21 +793,19 @@ static int8_t default_tg4_offsets[4][2] =
 nir_tex_instr *
 nir_tex_instr_create(nir_shader *shader, unsigned num_srcs)
 {
-   nir_tex_instr *instr = calloc(1, sizeof(*instr));
+   nir_tex_instr *instr = gc_zalloc(shader->gctx, nir_tex_instr, 1);
    instr_init(&instr->instr, nir_instr_type_tex);
 
    dest_init(&instr->dest);
 
    instr->num_srcs = num_srcs;
-   instr->src = malloc(sizeof(nir_tex_src) * num_srcs);
+   instr->src = gc_alloc(shader->gctx, nir_tex_src, num_srcs);
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i].src);
 
    instr->texture_index = 0;
    instr->sampler_index = 0;
    memcpy(instr->tg4_offsets, default_tg4_offsets, sizeof(instr->tg4_offsets));
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -843,8 +815,7 @@ nir_tex_instr_add_src(nir_tex_instr *tex,
                       nir_tex_src_type src_type,
                       nir_src src)
 {
-   nir_tex_src *new_srcs = calloc(sizeof(*new_srcs),
-                                         tex->num_srcs + 1);
+   nir_tex_src *new_srcs = gc_zalloc(gc_get_context(tex), nir_tex_src, tex->num_srcs + 1);
 
    for (unsigned i = 0; i < tex->num_srcs; i++) {
       new_srcs[i].src_type = tex->src[i].src_type;
@@ -852,7 +823,7 @@ nir_tex_instr_add_src(nir_tex_instr *tex,
                          &tex->src[i].src);
    }
 
-   free(tex->src);
+   gc_free(tex->src);
    tex->src = new_srcs;
 
    tex->src[tex->num_srcs].src_type = src_type;
@@ -888,13 +859,11 @@ nir_tex_instr_has_explicit_tg4_offsets(nir_tex_instr *tex)
 nir_phi_instr *
 nir_phi_instr_create(nir_shader *shader)
 {
-   nir_phi_instr *instr = malloc(sizeof(*instr));
+   nir_phi_instr *instr = gc_alloc(shader->gctx, nir_phi_instr, 1);
    instr_init(&instr->instr, nir_instr_type_phi);
 
    dest_init(&instr->dest);
    exec_list_make_empty(&instr->srcs);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -912,7 +881,7 @@ nir_phi_instr_add_src(nir_phi_instr *instr, nir_block *pred, nir_src src)
 {
    nir_phi_src *phi_src;
 
-   phi_src = calloc(1, sizeof(nir_phi_src));
+   phi_src = gc_zalloc(gc_get_context(instr), nir_phi_src, 1);
    phi_src->pred = pred;
    phi_src->src = src;
    phi_src->src.parent_instr = &instr->instr;
@@ -924,12 +893,10 @@ nir_phi_instr_add_src(nir_phi_instr *instr, nir_block *pred, nir_src src)
 nir_parallel_copy_instr *
 nir_parallel_copy_instr_create(nir_shader *shader)
 {
-   nir_parallel_copy_instr *instr = malloc(sizeof(*instr));
+   nir_parallel_copy_instr *instr = gc_alloc(shader->gctx, nir_parallel_copy_instr, 1);
    instr_init(&instr->instr, nir_instr_type_parallel_copy);
 
    exec_list_make_empty(&instr->entries);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -939,12 +906,10 @@ nir_ssa_undef_instr_create(nir_shader *shader,
                            unsigned num_components,
                            unsigned bit_size)
 {
-   nir_ssa_undef_instr *instr = malloc(sizeof(*instr));
+   nir_ssa_undef_instr *instr = gc_alloc(shader->gctx, nir_ssa_undef_instr, 1);
    instr_init(&instr->instr, nir_instr_type_ssa_undef);
 
    nir_ssa_def_init(&instr->instr, &instr->def, num_components, bit_size);
-
-   list_add(&instr->instr.gc_node, &shader->gc_list);
 
    return instr;
 }
@@ -1264,14 +1229,13 @@ void nir_instr_free(nir_instr *instr)
 
    switch (instr->type) {
    case nir_instr_type_tex:
-      free(nir_instr_as_tex(instr)->src);
+      gc_free(nir_instr_as_tex(instr)->src);
       break;
 
    case nir_instr_type_phi: {
       nir_phi_instr *phi = nir_instr_as_phi(instr);
-      nir_foreach_phi_src_safe(phi_src, phi) {
-         free(phi_src);
-      }
+      nir_foreach_phi_src_safe(phi_src, phi)
+         gc_free(phi_src);
       break;
    }
 
@@ -1279,8 +1243,7 @@ void nir_instr_free(nir_instr *instr)
       break;
    }
 
-   list_del(&instr->gc_node);
-   free(instr);
+   gc_free(instr);
 }
 
 void
@@ -1668,11 +1631,12 @@ nir_instr_move_src(nir_instr *dest_instr, nir_src *dest, nir_src *src)
 void
 nir_if_rewrite_condition(nir_if *if_stmt, nir_src new_src)
 {
+   nir_shader *shader = ralloc_parent(if_stmt);
    nir_src *src = &if_stmt->condition;
    assert(!src_is_valid(src) || src->parent_if == if_stmt);
 
    src_remove_all_uses(src);
-   src_copy(src, &new_src);
+   src_copy(src, &new_src, shader->gctx);
    src_add_all_uses(src, NULL, if_stmt);
 }
 